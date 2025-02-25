8e5b04b6bce6c18e89a6459bf187829823ce0ff2
laristizabal02 <lauris.paz@hotmail.es>
2025-02-24 20:50:02 -0700

    reorganize Client, adding Utils, API, auth and mutation... also, changing login to use mutation

---
+++ client/src/utils/mutation.ts
@@ -0,0 +1,26 @@
+import { gql } from '@apollo/client';
+
+// Mutation for adding a user
+export const ADD_USER = gql`
+  mutation AddUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
+      id
+      name
+      email
+    }
+  }
+`;
+
+// Mutation for logging in a user
+export const LOGIN_USER = gql`
+  mutation LoginUser($email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      user {
+        id
+        name
+        email
+      }
+    }
+  }
+`;
